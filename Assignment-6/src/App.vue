5e02396ddfbe9ed7f5319eef12b8ebf470e45c28
Jimit-bacancy <79782026+Jimit-bacancy@users.noreply.github.com>
2021-02-28 18:49:26 +0530

    Added Assignment-6

---
+++ Assignment-6/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <active-user :username="user.name" :userage="user.age"></active-user>
+        <user-data @set-data="setUserData"></user-data>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            user:{
+                name:'',
+                age: ''
+            }
+        }
+    },
+    methods:{
+        setUserData(name1,age1){
+            this.user = {
+                name: name1,
+                age: +age1
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+html{
+    font-family: sans-serif;
+}
+
+section{
+    margin: 2rem auto;
+    max-width: 40rem;
+    border-radius: 12px;
+    border: 1px solid #ccc;
+    padding: 1rem;
+}
+</style>
---
+++ Assignment-6/src/components/UserData.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData">
+            <input type="text" placeholder="Enter Name" v-model="enteredName">
+            <input type="text" placeholder="Enter Age" v-model="enteredAge">
+            <button>Set Data</button>
+        </form>
+        
+    </section>
+</template>
+
+<script>
+export default{
+    emits:['set-data'],
+    data(){
+        return {
+            enteredName:'',
+            enteredAge: '',
+        };
+    },
+    methods:{
+        submitData(){
+            this.$emit('set-data',this.enteredName,this.enteredAge);
+        }
+    }
+}
+</script>
