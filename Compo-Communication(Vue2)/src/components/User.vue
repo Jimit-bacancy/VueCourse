9d6606b1141c9d21dafb74bbd0fb1cfdebb501dd
Jimit-bacancy <79782026+Jimit-bacancy@users.noreply.github.com>
2021-02-28 19:27:58 +0530

    Section-30 Components Communication(Vue2)

---
+++ Compo-Communication(Vue2)/src/components/User.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change Name</button>
+        <p>Name = {{ name }}</p>
+        <p>Age = {{ age }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :name="name" @nameIsReset="name = $event" :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit :userAge="age" @ageIsEdited="age = $event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        },
+        data() {
+            return {
+                name: 'Max',
+                age: 24,
+            };
+        },
+        methods: {
+            changeName(){
+                this.name = 'Maxmillian';
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ Compo-Communication(Vue2)/src/components/UserDetail.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+import { eventBus } from '../main';
+export default{
+    props: ['name', 'userAge'],
+    methods: {
+        resetName(){
+            this.name = 'Max';
+            this.$emit('nameIsReset', this.name);
+        }
+    },
+    created(){
+        eventBus.$on('ageIsEdited', (age) => {
+            this.userAge = age;
+        });
+    },
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
