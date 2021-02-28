581caf47ae1e6af3a55d41df1447636e8ab0f73d
Jimit-bacancy <79782026+Jimit-bacancy@users.noreply.github.com>
2021-02-28 19:26:09 +0530

    Section-20 reuse and mixins

---
+++ reuse/src/components/DeleteUser.vue
@@ -0,0 +1,26 @@
+<template>
+  <user-alert v-if="alertIsVisible" :title="title" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+</template>
+
+<script>
+import alertMixins from '../mixins/alert';
+import UserAlert from './UserAlert';
+
+export default {
+  components: {
+    UserAlert,
+  },
+  data() {
+    return {
+      title: 'Delete User?????',
+    }
+  },
+  mixins: [alertMixins],
+};
+</script>
